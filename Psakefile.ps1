7ffc1d055892b14e1dfb75c378165b77ca3a7059
Craig Buchanan <craig.buchanan@choa.org>
2022-10-26 10:02:33 -0500

    adding publish task adding ghcr tag to build task

--- Psakefile.ps1
+++ Psakefile.ps1
@@ -13,6 +13,7 @@ Task Build {
         --build-arg "SBT_VERSION=$SBT_VERSION" `
         --build-arg "GIT_VERSION=$GIT_VERSION" `
         --build-arg "JDK_VERSION=$JDK_VERSION" `
+        --tag "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME" `
         --tag "$APP_NAME`:latest" `
         .
 }
@@ -20,4 +21,11 @@ Task Build {
 Task Terminal {
 	Write-Host 'Starting terminal...'
     docker run -it --rm "$APP_NAME`:latest" powershell
+}
+
+Task Publish {
+	Write-Host 'Publishing image to Github...'
+	$env:GITHUB_TOKEN | docker login ghcr.io -u $env:GITHUB_ACCOUNT --password-stdin
+    docker tag $APP_NAME "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME"
+	docker push "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME`:latest"
 }
